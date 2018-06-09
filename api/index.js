7c93e8c5d00633b694d7e7c7a12947b515d12df0
SeanCramsey <cramseys@oregonstate.edu>
2018-06-09 13:29:21 -0700

    Modified Routes in index.js, Added empty files for required routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 const router = module.exports = require('express').Router();
 
-router.use('/businesses', require('./businesses').router);
-router.use('/reviews', require('./reviews').router);
-router.use('/photos', require('./photos').router);
+//router.use('/businesses', require('./businesses').router);
+//router.use('/reviews', require('./reviews').router);
+//router.use('/photos', require('./photos').router);
 router.use('/users', require('./users').router);
+router.use('/games', require('./games').router);
+router.use('/consoles', require('./consoles').router);
+router.use('/rentals', require('./rentals').router);
+router.use('/console_reviews', require('./console_reviews').router);
+router.use('/game_reviews', require('./game_reviews').router);
