a9cae3db1a575a627eaa94318380773756cb7214
CelticChristoph <celticchristoph@live.com>
2018-06-13 16:22:59 -0700

    General layout for console and console review endpoints.

--- api/console_reviews.js
+++ api/console_reviews.js
@@ -1,8 +1,69 @@
 const router = require('express').Router();
 const validation = require('../lib/validation');
 
-
-
-
-
 exports.router = router;
+
+// Verification schema for consoles
+const addConsoleReviewSchema = {
+    consoleID:  { required: true },
+    userID:     { required: true },
+    rating:     { required: true },    
+    review:     { required: true }
+};
+
+const updateConsoleReviewSchema = {
+    consoleID:  { required: true },
+    userID:     { required: true },
+    rating:     { required: true },    
+    review:     { required: false }
+};
+
+// Check if user has reviewed this console previously.
+function existingReviewByUser(userID, consoleID, pool) {
+
+}
+
+// Add a new console to the consoles SQL database.
+function insertNewConsoleReview(info, pool) {
+    
+}
+
+// Route for posting a new console review.
+router.post('/', function (req, res, next) {
+
+});
+
+// Updates console review info by ID.
+function updateConsoleReviewByID(id, info, pool){
+
+} 
+
+// Route to update a new console by consoleID
+router.put('/:consoleID', function(req, res, next) {
+
+});
+
+// Delete console by consoleID.
+function deleteConsoleReviewByID(id, pool) {
+
+}
+
+// Route to delete a game.
+router.delete('/:consoleID', function (req, res, next) {
+
+});
+
+// Function to retrieve information about a specific console.
+function getConsoleReviewByID(id, pool) {
+
+}
+
+// Route for getting a specific console by consoleID
+router.get('/:consoleID', function (req, res, next) {
+
+});
+
+// Route for list of consoles.
+router.get('/', function(req, res) {
+
+});
--- api/consoles.js
+++ api/consoles.js
@@ -1,8 +1,75 @@
 const router = require('express').Router();
 const validation = require('../lib/validation');
 
-
-
-
-
 exports.router = router;
+
+// Verification schema for consoles
+const addConsoleSchema = {
+    name:           { required: true },
+    price:          { required: true },
+    tvrequirements: { required: true }
+};
+
+const updateConsoleSchema = {
+    name:           { required: false },
+    price:          { required: false },
+    tvrequirements: { required: false }
+};
+
+
+// Add a new console to the consoles SQL database.
+function insertNewConsole(info, pool) {
+    
+}
+
+// Route for posting a new console.
+router.post('/', function (req, res, next) {
+
+});
+
+// Updates console info by ID.
+function updateConsoleByID(id, info, pool){
+
+} 
+
+// Route to update a new console by consoleID
+router.put('/:consoleID', function(req, res, next) {
+
+});
+
+// Delete console by consoleID.
+function deleteConsoleByID(id, pool) {
+
+}
+
+// Route to delete a game.
+router.delete('/:consoleID', function (req, res, next) {
+
+});
+
+// Function to retrieve information about a specific console.
+function getConsoleByID(id, pool) {
+
+}
+
+// Route for getting a specific console by consoleID
+router.get('/:consoleID', function (req, res, next) {
+
+});
+
+
+// Support functions for getting paginated console list.
+// Get a console count, to help with pagincation.
+function getConsoleCount(pool){
+
+}
+
+// Get a page for the paginated list.
+function getConsolePage(page, totalCount, pool){
+
+}
+
+// Route for paginated list of consoles.
+router.get('/', function(req, res) {
+
+});
